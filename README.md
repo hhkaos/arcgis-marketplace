f7e76b01549b5b4f60a50451797a834f1bdf7478
RaulJimenezOrtega <hhkaos@gmail.com>
2017-05-24 10:01:52 +0200

    Adding new enpoints and refactoring

--- README.md
+++ README.md
@@ -4,5 +4,12 @@
 * [detail-page](https://hhkaos.github.io/domake-front-test/detail-page.html)
 * [preferences](https://hhkaos.github.io/domake-front-test/preferences.html)
 * [paypal](https://hhkaos.github.io/domake-front-test/paypal.html)
+* [items](https://hhkaos.github.io/domake-front-test/items.html)
+* [groups](https://hhkaos.github.io/domake-front-test/groups.html)
 
 API Documentation: https://gist.github.com/mongkok/dc33fcf333458b58489333f5981d9b48
+
+TODOs:
+* Add ArcGIS REST endpoint to documentation (correlations)
+* Marketplace permission?
+* Test all endpoints
---
+++ js/index.js
@@ -0,0 +1,25 @@
+$.ajaxSetup({
+  crossDomain: true,
+  xhrFields: {
+    withCredentials: true
+  }
+});
+
+var render_to_response = function(url, data){
+  var endpoint = "<hr><h2>Endpoint: " + url + "</h2>";
+  $(".result").append(endpoint + "<p>" + JSON.stringify(data) + "</p>");
+}
+
+var request = function(url, method, data, success) {
+  $.ajax(url, {
+    method: method,
+    data: data,
+    dataType: 'json',
+    success: function(data){
+      render_to_response(url, data)
+    },
+    error: function(data) {
+      render_to_response(url, data);
+    }
+  });
+}
