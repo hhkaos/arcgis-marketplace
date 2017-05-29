7526b9b987b95981cfeebc75336df2b291f45259
Raul Jimenez Ortega <hhkaos@gmail.com>
2017-05-30 01:13:00 +0200

    Refactoring and adding cookies

--- js/index.js
+++ js/index.js
@@ -7,30 +7,16 @@ $.ajaxSetup({
   }
 });
 
-var render_to_response = function(url, data){
-  var endpoint = "<hr><h2>Endpoint: " + url + "</h2>";
-  $(".result").append(endpoint + "<pre>" + JSON.stringify(data, undefined, 2) + "</pre>");
-};
-
 var request = function(url, method, data, callback) {
   $.ajax(url, {
     method: method,
     data: data,
     dataType: 'json',
-    success: function(data){
-      render_to_response(url, data);
+    complete: function(data){
 
       if(typeof callback === 'function'){
         callback(data);
       }
-    },
-    error: function(data) {
-      render_to_response(url, data);
-
-      if(typeof callback === 'function'){
-        callback(data);
-      }
-
     }
   });
 };
@@ -39,3 +25,18 @@ var request = function(url, method, data, callback) {
 var is_authenticated = function(callback){
   request('https://arcgis.domake.io/api/me?format=json', 'GET', null, callback);
 };
+
+var logged = Cookies.get('login');
+if(!logged){
+  is_authenticated(function(data){
+
+    if(data.responseJSON.username){
+      Cookies.set('login', 'true');
+      $("#login").hide();
+      $("#logout").show();
+    }
+  });
+}else{
+  $("#login").hide();
+  $("#logout").show();
+}
