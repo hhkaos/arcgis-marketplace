725aae2814959ff17a63f5ad2d402364a94e6cc9
RaulJimenezOrtega <hhkaos@gmail.com>
2017-07-13 05:41:22 +0200

    Adding new deploy

--- docs/assets/js/landing.js
+++ docs/assets/js/landing.js
@@ -1,18 +1,28 @@
 'use strict';
 
 var render_to_response = function(url, data){
+
+  var template = $.templates("#appCard");
+
   data.results.forEach(function(elem, i){
     elem.description = elem.description? elem.description: 'Without description';
-    var price = (elem.price/100 > 0)? '$'+ (elem.price/100): 'Free';
-    var p = '<div class="card block trailer-1">'+
-    '<a href="product/?id='+elem.id+'"><img class="card-image" src="'+elem.image+'"></a>'+
-    '<div class="card-content">'+
-    '<h4 class="font-size-1 trailer-half"><a href="product/?id='+elem.id+'">'+elem.name+'</a></h4>'+
-    '<p class="trailer-0 font-size--1">'+elem.description+' <br> Author: '+elem.owner.username+' | Price: '+ price +'</p>'+
-    '</div>'+
-    '</div>';
+    
+    elem.price = (elem.price > 0)? '$'+ (elem.price / 100): 'Free';
 
-    $('#products').append(p);
+    // var p = '<div class="card block trailer-1">'+
+    // '<a href="product/?id='+elem.id+'"><img class="card-image" src="'+elem.image+'"></a>'+
+    // '<div class="card-content">'+
+    // '<h4 class="font-size-1 trailer-half"><a href="product/?id='+elem.id+'">'+elem.name+'</a></h4>'+
+    // '<p class="trailer-0 font-size--1">'+elem.description+' <br> Author: '+elem.owner.username+' | Price: '+ price +'</p>'+
+    // '</div>'+
+    // '</div>';
+
+
+
+    var htmlOutput = template.render(elem);
+
+    $('#product-list').prepend(htmlOutput);
+
   });
 }
 
